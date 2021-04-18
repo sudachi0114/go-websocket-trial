1d4c138604963eaa6a0ba84292ed0741b1bc3343
sudachi0114 <jnevariste@gmail.com>
2021-04-18 11:13:33 +0900

    setup server with net/http

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// fmt.Println("Hello, world!")
+	http.HandleFunc("/ping", handler)
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		panic(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("pong"))
 }
